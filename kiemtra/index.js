cde2800736174e6b01c02bcaaa5af62dc3390b80
T150870h <90712503+T150870h@users.noreply.github.com>
2022-04-02 21:46:20 +0700

    kiem tra

--- kiemtra/index.js
+++ kiemtra/index.js
@@ -2,10 +2,21 @@
 
 // Xây dựng website rút gọn link từ API cho sẵn (đọc tài liệu sau để lấy API: https://shrtco.de/docs/), Các chức năng như trong video minh hoạ https://youtu.be/hhdiS8TGiPQ
 import Main from "../mail.js";
+class view {
+    constructor() {
+          this.$container = document.createElement('div');
+          this.$mail = new Main();
+    }
+    render() {
+        this.$container.appendChild(this.$mail.render());
+        return this.$container; 
+    }
+}
 const main = document.getElementById('root');
-   const Main1 = new Main();
+const view1 = new view();
+main.appendChild(view1.render());
 
-main.appendChild(Main1.render());
+
 
 
 
--- kiemtra/login.js
+++ kiemtra/login.js
@@ -1,6 +1,6 @@
 class Login{
     constructor() {
-
+           
     }
     render() {
 
